af1baea21af8362c7484ff2ed0c491e9caf74fd9
taranvd <apple.vlad.taran@gmail.com>
2024-02-27 20:21:38 +0200

    feat: added file handing functionality and Swagger

---
+++ src/files/entities/file.entity.ts
@@ -0,0 +1,29 @@
+import { UserEntity } from 'src/users/entities/user.entity';
+import {
+  Column,
+  DeleteDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('files')
+export class FileEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  filename: string;
+
+  @Column()
+  originalName: string;
+
+  @Column()
+  size: number;
+
+  @ManyToOne(() => UserEntity, (user) => user.files)
+  user: UserEntity;
+
+  @DeleteDateColumn()
+  deleteAt?: Date;
+}
---
+++ src/files/files.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { CreateFileDto } from './dto/create-file.dto';
+import { UpdateFileDto } from './dto/update-file.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FileEntity } from './entities/file.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class FilesService {
+  constructor(
+    @InjectRepository(FileEntity)
+    private repository: Repository<FileEntity>,
+  ) {}
+
+  findAll() {
+    return this.repository.find();
+  }
+}
