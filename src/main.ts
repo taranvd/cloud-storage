802e00c462e630c88e9747c08a9c7ab36ec46e1e
taranvd <apple.vlad.taran@gmail.com>
2024-02-28 12:54:35 +0200

    fix: env file

--- src/main.ts
+++ src/main.ts
@@ -12,10 +12,16 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('Cloud storage')
+    .addBearerAuth()
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
+  SwaggerModule.setup('swagger', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   await app.listen(7777);
 }
